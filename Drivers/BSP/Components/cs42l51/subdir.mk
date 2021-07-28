e4c4bcd02c93535522583c61a07574fbe648228d
steven-l-heck <81631496+steven-l-heck@users.noreply.github.com>
2021-07-28 14:39:56 -0500

    Add files via upload

---
+++ Drivers/BSP/Components/cs42l51/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/Components/cs42l51/cs42l51.c 
+
+OBJS += \
+./Drivers/BSP/Components/cs42l51/cs42l51.o 
+
+C_DEPS += \
+./Drivers/BSP/Components/cs42l51/cs42l51.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/Components/cs42l51/cs42l51.o: ../Drivers/BSP/Components/cs42l51/cs42l51.c Drivers/BSP/Components/cs42l51/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L475xx -c -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/steve/STM32Cube/Repository/STM32Cube_FW_L4_V1.17.0/Drivers/BSP" -I"C:/Users/steve/STM32Cube/Repository/STM32Cube_FW_L4_V1.17.0/Drivers/BSP/B-L475E-IOT01" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/BSP/Components/cs42l51/cs42l51.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
