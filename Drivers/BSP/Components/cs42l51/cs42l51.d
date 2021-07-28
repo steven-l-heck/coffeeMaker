d84d52302d9f44556c37cc5980eaad803adc69b7
steven-l-heck <81631496+steven-l-heck@users.noreply.github.com>
2021-07-28 14:40:58 -0500

    Delete Drivers/BSP/Components/cs42l51 directory

--- Drivers/BSP/Components/cs42l51/cs42l51.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/cs42l51/cs42l51.o: \
- ../Drivers/BSP/Components/cs42l51/cs42l51.c \
- ../Drivers/BSP/Components/cs42l51/cs42l51.h \
- ../Drivers/BSP/Components/cs42l51/../Common/audio.h
-
-../Drivers/BSP/Components/cs42l51/cs42l51.h:
-
-../Drivers/BSP/Components/cs42l51/../Common/audio.h:
